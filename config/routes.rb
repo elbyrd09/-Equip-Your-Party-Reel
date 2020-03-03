9cde5be642179c0e5967d20194e107ec2738a1b6
jawad sarfraz <jadiman@gmail.com>
2020-03-03 14:44:49 +0700

    configured routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,16 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "dashboard", to: "pages#dashboard", as: :dashboard
+​
+  resources :items do
+    resources :pictures, only: [:create]
+    resources :rentals, only: [:create]
+  end
+​
+  resources :pictures, only: [:update, :destroy]
+  resources :rentals, only: [:show, :destroy] do
+    resources :reviews, only: [:create]
+  end
+
 end
