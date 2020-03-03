4c58cb52ff84bbd140ca696bfd68acf2fc73661b
jawad sarfraz <jadiman@gmail.com>
2020-03-03 12:38:02 +0700

    Fixed syntax errors in validations updated

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -3,7 +3,7 @@ class Rental < ApplicationRecord
   belongs_to :item
   has_one :review
 
-  validates :start_dat, presence: true
+  validates :start_date, presence: true
   validates :end_date, presence: true
   validates :total_price, presence: true
 
